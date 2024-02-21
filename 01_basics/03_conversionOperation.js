bc81a143b9d6cee4e042f5ed2b1172103791a341
Muskanmahi23 <93921249+Muskanmahi23@users.noreply.github.com>
2024-02-21 13:12:16 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let isString = "Hardik";
+
+let booleanIsString = Boolean(isString)
+console.log(booleanIsString);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
