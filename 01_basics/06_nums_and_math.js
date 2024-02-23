ce2662fde0506a70a3e476d75f26ef2c6a900fc4
Muskanmahi23 <93921249+Muskanmahi23@users.noreply.github.com>
2024-02-23 05:01:20 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8986
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math); // gives random values between 0 to 1 so mostly decimal values 
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
