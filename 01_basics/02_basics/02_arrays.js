25bfc0e33851d22292bf1c762721f2d7a0880675
Muskanmahi23 <93921249+Muskanmahi23@users.noreply.github.com>
2024-02-25 06:48:57 +0000

    Array methods and its uses

---
+++ 01_basics/02_basics/02_arrays.js
@@ -0,0 +1,34 @@
+const marvel_heroes = ["Thor", "Ironman", "Spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+// marvel_heroes.push(dc_heroes)
+
+//  console.log(marvel_heroes);
+
+// console.log(marvel_heroes[3]);
+// console.log(marvel_heroes[3][2]);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes);
+
+
+// const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heroes);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.isArray("Hardik"));
+// console.log(Array.from("Hardik"));
+
+// console.log(Array.from({name: "Hardik"})); // interesting situation gives output an empty array because It can't convert name into array
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
