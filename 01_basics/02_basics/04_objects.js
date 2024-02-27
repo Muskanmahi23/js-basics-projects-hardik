d20c1765aed63ce0e0222e861c4ca5d6e115e789
Muskanmahi23 <93921249+Muskanmahi23@users.noreply.github.com>
2024-02-27 05:48:29 +0000

    Oject explanation in js

---
+++ 01_basics/02_basics/04_objects.js
@@ -0,0 +1,54 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hardik",
+            lastname: "kumar"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b", 3: "c"}
+const obj2 = {4: "a", 5: "b", 6: "c"}
+
+// const obj3 = {obj1, obj2}
+const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    
+]
+
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
