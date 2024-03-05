128c80516ffd4c07f64f25b9cc7276874fee1f8f
Muskanmahi23 <93921249+Muskanmahi23@users.noreply.github.com>
2024-03-05 09:18:19 +0000

    maps and loops in js

---
+++ 05_iterations/three.js
@@ -0,0 +1,45 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello World!"
+
+// for (const greet of greetings) {
+//     // if (greet === " ") {
+
+//     //     continue
+//     // }
+//     console.log(`Each character is ${greet}`);
+// }
+
+// Maps
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+// for (const [key, value] of object) {
+//     console.log(key, ':-', value);
+// }
+
